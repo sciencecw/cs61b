82568a2cbb6c1ec05a11291de28ec32a20b0c569
kakwong <sciencecw@gmail.com>
2015-02-24 02:16:31 -0800

    hw4 completed

---
+++ hw4/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,71 @@
+// Make sure to make this class a part of the synthesizer package
+package synthesizer;
+
+public class ArrayRingBuffer extends AbstractBoundedQueue {
+  /* Index for the next dequeue or peek. */
+  private int first;           
+  /* Index for the next enqueue. */
+  private int next;             
+  /* Array for storing the buffer data. */
+  private double[] rb;
+
+  /** Create a new ArrayRingBuffer with the given capacity. */
+  public ArrayRingBuffer(int capacity) {
+    // TODO: Create new array with capacity elements.
+    //       first, last, and fillCount should all be set to 0. 
+    //       this.capacity should be set appropriately. Note that the local variable
+    //       here shadows the field we inherit from AbstractBoundedQueue.
+    this.rb = new double[capacity];
+    this.first = 0;
+    this.next = 0;
+    this.fillCount =0;
+    this.capacity = capacity;
+  }
+
+  /** Adds x to the end of the ring buffer. If there is no room, then
+    * throw new RuntimeException("Ring buffer overflow") 
+    */
+  public void enqueue(double x) {
+    // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+    // is there room?
+    if (isFull()){
+      throw new RuntimeException("Ring buffer overflow");
+    }else{
+      fillCount+=1;
+      rb[next]=x;
+      next+=1;
+      if (next>=capacity){
+        next=0;
+      }
+    }
+    return;
+  }
+
+  /** Dequeue oldest item in the ring buffer. If the buffer is empty, then
+    * throw new RuntimeException("Ring buffer underflow");
+    */
+  public double dequeue() {
+    // TODO: Dequeue the first item. Don't forget to decrease fillCount and update first.
+    if (isEmpty()){
+      throw new RuntimeException("Ring buffer overflow");
+    }
+    double temp;
+    temp = rb[first];
+    first+=1;
+    fillCount-=1;
+    if (first>=capacity){
+        first=0;
+    }
+    return temp;
+  }
+
+  /** Return oldest item, but don't remove it. */
+  public double peek() {
+    if (isEmpty()){
+      throw new RuntimeException("Ring buffer overflow");
+    }
+    return rb[first];
+    // TODO: Return the first item. None of your instance variables should change.
+  }
+
+}
--- hw4/synthesizer/TestArrayRingBuffer.java
+++ hw4/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,62 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+		assertEquals(arb.capacity(),10);          
+		assertEquals(arb.fillCount(),0);        
+		assertTrue(arb.isEmpty());       
+		assertFalse(arb.isFull());        
+		arb.enqueue(1.1);
+		arb.enqueue(2.2);
+		arb.enqueue(3.3);
+		arb.enqueue(4.4); 
+		assertEquals(arb.capacity(),10);          
+		assertEquals(arb.fillCount(),4);        
+		assertFalse(arb.isEmpty());       
+		assertFalse(arb.isFull());
+		assertEquals(arb.peek(),1.1,1e-6);  
+		assertEquals(arb.dequeue(),1.1,1e-6);
+		assertEquals(arb.fillCount(),3);         
+		assertEquals(arb.dequeue(),2.2,1e-6);
+		assertEquals(arb.dequeue(),3.3,1e-6);
+		assertEquals(arb.dequeue(),4.4,1e-6);
+		assertTrue(arb.isEmpty());  
+
+		//enqueue and dequeue 9 items      
+		for (int i=0;i<10;i++){
+			arb.enqueue(1.1);
+		}
+		assertTrue(arb.isFull());
+		for (int i=0;i<10;i++){
+			assertEquals(arb.dequeue(),1.1,1e-6);
+		}
+		assertTrue(arb.isEmpty());
+
+
+		// fill by 9 items
+		for (int i=0;i<9;i++){
+			arb.enqueue(i*i*1.1);
+		}
+		assertFalse(arb.isFull());
+		assertFalse(arb.isEmpty());
+		// peek millions times
+		for (int i=0;i<1000;i++){
+			assertEquals(arb.peek(),0.,1e-6);
+		}
+		System.out.println(arb.dequeue());
+		System.out.println(arb.dequeue());
+		// peek millions times
+		for (int i=0;i<1000;i++){
+			assertEquals(arb.peek(),4.4,1e-6);
+		}
+
+		//capacity();          
+		//fillCount();         
+		//isEmpty();       
+		//isFull();        
+		//enqueue(double x);  
+		//dequeue();        
+		//peek();           
     }
 
     /** Calls tests for ArrayRingBuffer. */
