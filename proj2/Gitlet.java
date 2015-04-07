b73fc6ae471a0f410b2dee0ad9675b71a045172d
kakwong <sciencecw@gmail.com>
2015-04-07 07:52:36 -0700

    proj2 first draft

---
+++ proj2/Gitlet.java
@@ -0,0 +1,173 @@
+import java.io.File;
+import java.util.Date;
+import java.io.IOException;
+import java.lang.ClassNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Gitlet {
+
+    private static State state;
+    private static String[] argm;
+
+    private static void init() {
+        if (isInit()){
+            System.out.println("A gitlet version control system already exists in the current directory.");
+        }else {
+            state = new State();
+            File f1 = new File(".Gitlet/");
+            if (!f1.exists()) {
+                f1.mkdir();
+            } else {
+                System.out.println("warning: gitlet folder already exists");
+            }
+            writeState();    
+        }
+    }
+
+    private static void add(String fileString) {
+        File f = new File(fileString);
+        boolean hasAdd = state.addFile(f); 
+        return;
+    }
+
+    private static void commit(String msg) {
+        state.commit(msg);
+    }
+    
+    private static void rm(String fileString) {
+        File f = new File(fileString);
+        boolean hasrm = state.removeFile(f); 
+        return;
+    }
+    private static void log() {
+        state.log();
+    }   
+    private static void globalLog() {
+        state.glog();
+    }   
+
+    private static void find() {
+
+    }   
+
+    private static void status() {
+
+    }   
+
+    private static void ckout(String s, int id) {
+
+    } 
+
+    private static void ckout(String s) {
+        state.checkout(s);
+    }   
+
+    private static void branch() {
+
+    }   
+
+    private static void rmBranch() {
+
+    }   
+
+    private static void reset() {
+
+    }   
+
+    //Helper methods
+    private static void writeState() {
+        try {
+            File sfile = new File(".Gitlet/State.ser");
+            FileOutputStream fos = new FileOutputStream(sfile);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            //System.out.println(state);
+            oos.writeObject(state);
+            oos.close();            
+        } catch (IOException e) {
+            System.out.println("fail to write state!");
+            System.out.println(e);
+        }
+    }
+
+    private static void readState() {
+        try {
+            File sfile = new File(".Gitlet/State.ser");
+            FileInputStream fis = new FileInputStream(sfile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Object sObj = ois.readObject();
+            state = (State) sObj;
+            ois.close();            
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("fail to read state");
+        }
+    }
+
+    private static boolean isInit() {
+        return new File(".Gitlet/State.ser").exists(); 
+    }
+
+    private static boolean checklength(int lg) {
+        return argm.length >= lg;
+    }
+
+    private static void danger(){
+        System.out.println("Warning: The command you entered may alter the files"
+         + "in your working directory. Uncommitted changes may be lost." 
+         + " Are you sure you want to continue? (yes/no)");
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String textLine = reader.readLine();
+            if (textLine.startsWith("yes")){
+                return;
+            } else {
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            System.out.println("error reading your response.");
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        argm = args;
+        if (!checklength(1)) {
+            System.out.println("not enough arguments");
+            return;
+        }
+        if ("init".equals(args[0])) {
+            init();
+            return;
+        }
+        if (isInit()) {
+            readState();
+            switch (args[0]) {
+                case "add": if (checklength(2)) add(args[1]);
+                            break;
+                case "commit": if (checklength(2)) commit(args[1]);
+                                else System.out.println("Please enter a commit message.");
+                            break;
+                case "rm": if (checklength(2)) rm(args[1]);
+                            break;
+                case "log": log();
+                            break;
+                case "global-log": globalLog();
+                            break;
+                case "checkout": danger();
+                        System.out.println(argm.length);
+                        if (checklength(3)) ckout(args[2],Integer.parseInt(args[1]));
+                        else if (checklength(2)) ckout(args[1]);
+                        break;
+            }
+            writeState();
+        } else {
+            System.out.println("Gitlet is not initialized.");
+        }
+    }
+}
---
+++ proj2/State.java
@@ -0,0 +1,217 @@
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Date;
+import java.io.IOException;
+import java.io.Serializable;
+import java.lang.RuntimeException;
+import java.text.SimpleDateFormat;
+import java.nio.file.StandardCopyOption;
+
+
+public class State implements Serializable {
+	protected Commit root;  // not sure how useful is this; might review later
+							// Stores the initial commit address
+	// List of IO addresses to the files to be copied
+	protected HashSet<File> addSet;
+	protected HashSet<File> rmSet;
+	// pointers to end of each branch as well as HEAD pointer
+	public HashMap<String,Commit> pointers;
+	// list of all commits according to their ID
+	protected ArrayList<Commit> commitList;
+	// commit id of the next commit
+	protected int cid = 0;
+
+	public State() {
+		root = new Commit();
+		addSet = new HashSet<File>();
+		rmSet = new HashSet<File>();
+		pointers = new HashMap<String, Commit>();
+		pointers.put("HEAD", root);
+		commitList = new ArrayList<Commit>();
+		commitList.add(root);
+		cid = 1;
+		return;
+	}
+
+	public boolean addFile(File f) {
+		if (f.exists() && f.isFile()) {
+			if (isModified(f)) {
+				addSet.add(f);
+				return true;
+			} else {
+				System.out.println("File has not been modified since the last commit.");
+			}
+			System.out.println("File does not exist, or the address is not a file.");
+		}
+		return false;
+	}
+
+	public void commit(String msg) {
+		// !!! case where no add occurs
+		if (addSet.isEmpty() && rmSet.isEmpty()) {
+			System.out.println("No changes added to the commit.");
+			return;
+		}
+		Commit prev = pointers.get("HEAD");
+		// make new dir, move files
+		HashMap<File,File> ads = commitHelper();
+		Commit c = new Commit(prev, cid, ads, msg);
+		commitList.add(c);
+		pointers.put("HEAD", c);
+		addSet.clear(); // clear add set
+		rmSet.clear();
+		cid += 1;		// update commitid
+	}
+
+	public boolean removeFile(File f) {
+		if (!f.exists()) {
+			System.out.println("File does not exist.");
+			return false;
+		}
+		if (!f.isFile()) {
+			System.out.println("This is not a file.");
+			return false;
+		}
+		boolean isRemoved = false;
+		if (addSet.contains(f)) {
+			addSet.remove(f);
+			isRemoved = true;
+		} 
+		HashMap<File,File> oldmap = getAdss("HEAD");
+		if (oldmap.containsKey(f)) {
+			rmSet.add(f);
+			isRemoved = true;
+		}
+		if (isRemoved) {
+			return true;
+		}
+		System.out.println("No reason to remove the file.");
+		return false;
+	}
+
+	public void log() {
+		Commit ptr = pointers.get("HEAD");
+		for (;ptr != null; ptr = ptr.getPrev()) {
+			printcommit(ptr);
+		}
+		return;
+	}
+
+	public void glog() {
+		for (Commit c: commitList) {
+			printcommit(c);
+		}
+		return;
+	}
+
+	public void checkout(String s){
+		// only file case for now
+		File fnow = new File(s);
+		File fi = getheadFile(fnow);
+		try {
+			// replace current file by old file
+			System.out.println("checkout system exit");
+			Files.copy(fi.toPath(),fnow.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println(e);
+			System.out.println("checkout failure");
+		}
+	}
+
+
+
+	// HELPER METHODS
+
+	// Fix this!
+	private boolean isModified(File f) {
+		File prevFile = getheadFile(f);
+		if (prevFile == null) {
+			return true;
+		}
+		try {
+			byte[] encodeNew = Files.readAllBytes(f.toPath());
+			byte[] encoded = Files.readAllBytes(getheadFile(f).toPath()); // !!!what's file name?
+			if (encoded.length != encodeNew.length) {
+				return true;
+			}
+			for (int i = 0; i < encodeNew.length; i++) {
+				if (encoded[i] != encodeNew[i]) {
+					return true;
+				}
+			}
+		} catch (IOException | SecurityException e) {
+			System.out.println("Check modified: Exception reading file");
+			System.out.println(e);
+			System.exit(1);
+		}
+		return false;
+	}
+
+	// get the address of immediate previous file at the HEAD pointer
+	// may be different from current file since it may be changed
+	private File getheadFile(File f) {
+		Commit c = pointers.get("HEAD");
+		File ff = c.getAddress(f);
+		return ff;
+	}
+
+	private HashMap<File,File> getAdss(String ptr) {
+		Commit c = pointers.get(ptr);
+		HashMap<File,File> ff = c.getAddresses();
+		return ff;
+	}
+
+	// use info in addSet and rmSet
+	// return fileAddress map and copy files
+	private HashMap<File,File> commitHelper() {
+		HashMap<File,File> remap = new HashMap<File,File>();
+		HashMap<File,File> oldmap = getAdss("HEAD");
+		remap.putAll(oldmap); // put all old addresses in
+		// create new folder
+		File folder = new File(".Gitlet/" + commitName(cid) + "/");
+		if (folder.exists()) {
+			throw new RuntimeException("Gitlet folder already exist");
+		} else {
+			folder.mkdir();
+		}
+		// add all addset stuff into new commit
+		for (File fi: addSet) {
+			File ff = new File(folder, fi.getPath());
+			remap.put(fi, ff);
+			if (!ff.getParentFile().exists()) {
+				ff.getParentFile().mkdirs();
+			}
+			//copy files into gitlet folder
+			try {
+				Files.copy(fi.toPath(),ff.toPath());
+			} catch (IOException e) {
+				System.out.println(e);
+				System.out.println("Potential commit failure");
+			}
+		}
+		for (File fi: rmSet) {
+			remap.remove(fi);
+		}
+		return remap;
+	}
+
+	private File getGitletfolder(int id) {
+		return new File(".Gitlet/" + commitName(id) + "/");
+	}
+
+	private String commitName(int num) {
+		return String.format("%05d",num);
+	}
+
+	private void printcommit(Commit c) {
+		System.out.println("====");
+		System.out.println("Commit " + c.getID() + ".");
+		System.out.println(new SimpleDateFormat(
+				"YYYY-MM-dd  HH:mm:ss").format(c.getDate()));
+		System.out.println(c.getMessage());
+		System.out.println();
+	}
+}
